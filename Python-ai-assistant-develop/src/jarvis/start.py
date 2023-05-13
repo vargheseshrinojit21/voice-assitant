6ddc944f6955cfab07ce7c6d3e80b142cb5a9233
vargheseshrinojit21 <95203069+vargheseshrinojit21@users.noreply.github.com>
2023-05-13 08:52:57 +0530

    Add files via upload

---
+++ Python-ai-assistant-develop/src/jarvis/start.py
@@ -0,0 +1,47 @@
+# MIT License
+
+# Copyright (c) 2019 Georgios Papachristou
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from jarvis import settings
+from jarvis.utils.startup import internet_connectivity_check
+from jarvis.core.processor import Processor
+from jarvis.core.console import ConsoleManager
+
+
+def main():
+    """
+    Do initial checks, clear the console and print the assistant logo.
+    """
+
+    console_manager = ConsoleManager()
+    console_manager.console_output(info_log='Wait a second for startup checks..')
+    internet_connectivity_check()
+    console_manager.console_output(info_log='Application started')
+    console_manager.console_output(info_log="I'm ready! Say something :-)")
+    processor = Processor(console_manager=console_manager, settings_=settings)
+
+    while True:
+        processor.run()
+
+
+if __name__ == '__main__':
+    main()
+
